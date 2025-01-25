0cc0b7402d41921ce4b6660cf42916b9eb04e21c
basleal22 <solibaslu@gmail.com>
2025-01-25 18:38:19 +0300

    added aggregation function for numerical and categorical columns

--- scripts/feature_eng.py
+++ scripts/feature_eng.py
@@ -3,5 +3,12 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import seaborn as sns
-def aggregate(data):
+def num_aggregate(data):
+    num_cols= data.select_dtypes(include=['int64','float64']).columns.tolist()
+    num_agg= data.groupby('CustomerId')[num_cols].agg('mean','sum','max','min').reset_index()
+    return num_agg
+def cat_aggregate(data):
+    cat_cols=data.select_dtypes(include=['object']).columns.tolist()
+    cat_agg=data.groupby('CustomerId')[cat_cols].agg(lambda x:x.mode().iloc[0] if not x.mode().empty else None)
+    return cat_agg
     
