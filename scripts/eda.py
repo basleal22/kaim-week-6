395dc9647468a3975daba03849728bd18c2d36ad
basleal22 <solibaslu@gmail.com>
2025-01-25 10:10:09 +0300

    edited numerical feature histogram to look more clean

--- scripts/eda.py
+++ scripts/eda.py
@@ -20,24 +20,26 @@ def visualization(data):
         plt.show()
 def visual_num(data):
     # Identify numerical columns
-    numerical_columns = data.select_dtypes(include=['float64', 'int64']).columns
+    #numerical_columns = data.select_dtypes(include=['float64', 'int64']).columns
+    df_num = data.select_dtypes(include = ['float64', 'int64'])
+    df_num.hist(figsize = (15, 10), bins = 50);
 
 # Visualize distributions
-    for col in numerical_columns:
-        plt.figure(figsize=(8, 5))
-        sns.histplot(data[col], kde=True, bins=30, color='blue')
-        plt.title(f"Distribution of {col}")
-        plt.xlabel(col)
-        plt.ylabel("Frequency")
-        plt.show()
+    #for col in numerical_columns:
+        #plt.figure(figsize=(8, 5))
+        #sns.histplot(data[col], kde=True, bins=30, color='blue')
+        #plt.title(f"Distribution of {col}")
+        #plt.xlabel(col)
+        #plt.ylabel("Frequency")
+        #plt.show()
 
 # Boxplots for outlier detection
-    for col in numerical_columns:
-        plt.figure(figsize=(8, 5))
-        sns.boxplot(x=data[col], color='orange')
-        plt.title(f"Boxplot of {col}")
-        plt.xlabel(col)
-        plt.show()
+    #for col in numerical_columns:
+        #plt.figure(figsize=(8, 5))
+        #sns.boxplot(x=data[col], color='orange')
+        #plt.title(f"Boxplot of {col}")
+        #plt.xlabel(col)
+        #plt.show()
 def visual_cat(data):
         # Identify categorical columns
     categorical_columns = data.select_dtypes(include=['object']).columns
